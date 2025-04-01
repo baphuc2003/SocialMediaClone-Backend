8bbc7768eb64439889eae78b5f41f109b684fcd6
baphuc <164721811+baphuc2003@users.noreply.github.com>
2025-04-01 20:13:03 +0700

    Add debug logs for environment variables

--- dist/app.module.js
+++ dist/app.module.js
@@ -32,24 +32,47 @@ exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
         imports: [
-            typeorm_1.TypeOrmModule.forRoot({
-                type: "mysql",
-                host: "localhost",
-                port: 3306,
-                username: "root",
-                password: "@314159Phuc",
-                database: "demo",
-                synchronize: true,
-                autoLoadEntities: true,
+            typeorm_1.TypeOrmModule.forRootAsync({
+                imports: [config_1.ConfigModule],
+                useFactory: (configService) => {
+                    console.log("DATABASE_HOST:", configService.get("DATABASE_HOST"));
+                    console.log("DATABASE_PORT:", configService.get("DATABASE_PORT"));
+                    return {
+                        type: "mysql",
+                        host: configService.get("DATABASE_HOST") || "localhost",
+                        port: +configService.get("DATABASE_PORT") || 3306,
+                        username: configService.get("DATABASE_USERNAME") || "root",
+                        password: configService.get("DATABASE_PASSWORD") || "@314159Phuc",
+                        database: configService.get("DATABASE_NAME") || "demo",
+                        synchronize: true,
+                        autoLoadEntities: true,
+                    };
+                },
+                inject: [config_1.ConfigService],
             }),
-            mongoose_1.MongooseModule.forRoot("mongodb://localhost/nest"),
+            mongoose_1.MongooseModule.forRootAsync({
+                imports: [config_1.ConfigModule],
+                useFactory: (configService) => {
+                    console.log("MONGO_HOST:", configService.get("MONGO_HOST"));
+                    console.log("MONGO_PORT:", configService.get("MONGO_PORT"));
+                    console.log("MONGO_DATABASE:", configService.get("MONGO_DATABASE"));
+                    return {
+                        uri: `mongodb://${configService.get("MONGO_HOST") || "localhost"}:${configService.get("MONGO_PORT") || 27017}/${configService.get("MONGO_DATABASE") || "nest"}`,
+                    };
+                },
+                inject: [config_1.ConfigService],
+            }),
             users_module_1.UsersModule,
             mail_module_1.MailModule,
-            bullmq_1.BullModule.forRoot({
-                connection: {
-                    host: "localhost",
-                    port: 6379,
-                },
+            bullmq_1.BullModule.forRootAsync({
+                imports: [config_1.ConfigModule],
+                useFactory: (configService) => ({
+                    connection: {
+                        host: configService.get("REDIS_HOST") || "localhost",
+                        port: +configService.get("REDIS_PORT") || 6379,
+                    },
+                }),
+                inject: [config_1.ConfigService],
             }),
             public_key_module_1.PublicKeyModule,
             auth_module_1.AuthModule,
